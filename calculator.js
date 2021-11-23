cc1e5b76ae61144a17e0a3aa95decfa0a035831f
Manahil Ameer <manahil1054@gmail.com>
2021-11-23 08:44:57 +0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,17 +1,19 @@
+// Function to clear the screen
 function Clear(){
     document.getElementById('result').value="";
 }
+// Function to display the given value on screen 
 function display(value){
     document.getElementById('result').value+=value;
 }
+// Function to Compute the written expression 
 function compute(){
     var demo=document.getElementById('result').value;
-    
+    // Error handling
     try{
         var res = eval(demo);
     }
     catch(e){
-        // document.getElementById("result").style.backgroundColor='red';
         res = "Invalid Expression!!";
     }
     document.getElementById("result").value = res;

a63ccc37135bc2546ffa313f8e2f9461f63ae44a
Manahil Ameer <manahil1054@gmail.com>
2021-11-27 21:22:40 +0500

    Fixed Issues

--- calculator.js
+++ calculator.js
@@ -9,30 +9,55 @@ function display(value) {
 function compute() {
   const input = document.getElementById("output-screen").value;
   let output;
-  if(input==""){
-      output="Enter something";
-      setTimeout(function () {
-        clearScreen();
-      }, 500);
-  }
-  else{
-  try {
-    output = eval(input);
+  if (input == "") {
+    output = "Enter something";
     setTimeout(function () {
       clearScreen();
-    }, 500);
-  } catch (e) {
-    output = "Invalid Expression!";
-    setTimeout(function(){
+    }, 700);
+  } else {
+    try {
+      output = eval(input);
+    } catch (e) {
+      output = "Invalid Expression!";
+      setTimeout(function () {
         clearScreen();
-    },500);
-  }}
+      }, 700);
+    }
+  }
   document.getElementById("output-screen").value = output;
 }
-
 document.onkeydown = function (ev) {
+  if (ev.key >= "0" && ev.key <= "9") {
+    ev.preventDefault();
+    // compute();
+    display(ev.key);
+  }
+  if (ev.key == "+" || ev.key == "/" || ev.key == "-" || ev.key == "*") {
+    ev.preventDefault();
+    // compute();
+    display(ev.key);
+  }
   if (ev.key == "Enter") {
     ev.preventDefault();
     compute();
   }
+  if (ev.key == "Backspace") {
+    clearScreen();
+  }
 };
+
+const buttons = document.querySelectorAll("button");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", (ev) => {
+    if (buttons[i].textContent == "=" ) {
+      compute();
+    }
+    if( buttons[i].textContent == "C"){
+      clearScreen();
+    }
+    else if (buttons[i].textContent != "=" && buttons[i].textContent!="C") {
+      display(buttons[i].textContent);
+    }
+    
+  });
+}
